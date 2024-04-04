e851aaee2f3460495b86b2f209d726bd9e5dc372
Rad-spencer <159556435+Rad-spencer@users.noreply.github.com>
2024-04-04 05:38:57 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+//alert ("hello")// we are using node.js not browser
+
+/*console.log(3+3) 
+console.log("Raj") 
+code readablity should be high like above */
+
+/*let name = "Raj"
+Let age = 24
+let isloggedin = false
+Let State;*/
+
+/* types of data types
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+object 
+*/
+//console.log (typeof null); output= object
+//console.log(typeof undefine); output= undefined
