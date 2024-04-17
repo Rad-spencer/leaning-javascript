2b9da2d1ea6333826f15bc99075e746f8533d240
Rad-spencer <159556435+Rad-spencer@users.noreply.github.com>
2024-04-17 02:03:08 +0000

    dates in javascript

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,13 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getTimezoneOffset());
