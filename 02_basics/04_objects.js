92b163de77af1b281e21590e83bca096a368459b
Rad-spencer <159556435+Rad-spencer@users.noreply.github.com>
2024-04-25 11:35:29 +0000

    objects indepth

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,7 +21,11 @@ const regularUser = {
 
 const obj1 = {1:"a", 2:"b"}
 const obj2 = {3:"c", 4:"d"}
-const obj3 = Object.assign(obj1,obj2)
-const obj4 = Object.assign({},obj1,obj2)// both obj3 and obj4 has the same result but the difference on both of that is obj4 is best practice because when object is assign the first is the target and rest all will be souce so due to that we give an empty array to act as target so that all the source are stored on empty or else as in obj3 all the souce is stored in obj1 although both has same output but according to syntax obj4 is the best practice
-console.log(obj3);
-console.log(obj4);
+// const obj3 = Object.assign(obj1,obj2)
+// const obj4 = Object.assign({},obj1,obj2)// both obj3 and obj4 has the same result but the difference on both of that is obj4 is best practice because when object is assign the first is the target and rest all will be souce so due to that we give an empty array to act as target so that all the source are stored on empty or else as in obj3 all the souce is stored in obj1 although both has same output but according to syntax obj4 is the best practice and even this method is least used or can be said not frequently used
+// console.log(obj3);
+// console.log(obj4);
+
+const obj3 = {...obj1,...obj2}// this is seprator operator and this is used most of the time
+//console.log(obj3);
+
