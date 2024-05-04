4b1759798a9399a066444a3746b8245c055883db
Rad-spencer <159556435+Rad-spencer@users.noreply.github.com>
2024-05-04 08:32:00 +0000

    IIFE -- Named and Unnamed IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -4,15 +4,29 @@
 //     console.log(`DB CONNECTED`);
 // }
 // chai()
+
+
+/*this is named IIFE*/
 (function chai(){
     console.log(`DB CONNECTED`);
-}()); // using () on the function can reduce the global pollution and to exicute the function one more () will required ()(). the first () is for function (function) and second one is for exicution which make it like (function)(this will be exicution which is used on chai after the function and scope for exicutation call) which will look like (function)()
+}()); 
+// using () on the function can reduce the global pollution and to exicute the function one more () will required ()(). the first () is for function (function) and second one is for exicution which make it like (function)(this will be exicution which is used on chai after the function and scope for exicutation call) which will look like (function)()
+
+
 
 // this can be used as arrow functions also like
 
+
+
+/*this is simple IIFE*/
 (() => {
 console.log(`DB CONNECTED TWO`);
-})()
+})();
 
 //previously there was an error this was because to use the arrow function like this method is allowed only when the previous line should end with ;
 
+
+// if you need to add name then do as function
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    })('Raj')
