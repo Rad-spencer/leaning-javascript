17c1af65a245e14c80c1123352166b473e6e3056
Rad-spencer <159556435+Rad-spencer@users.noreply.github.com>
2024-05-07 10:35:45 +0000

    for-in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,11 @@
+const myObject = {
+    js: 'Javascript',
+    Cpp: 'C++',
+    Rb: "ruby",
+    swift: "Swift by apple"
+}
+for (const key in myObject) {
+   console.log(`${key} shortcut is for ${myObject[key]}`);
+        
+    }
+
